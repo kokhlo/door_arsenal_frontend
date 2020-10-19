3d01fd2c806886b87c47518ee865aeb092045d64
Konstantin Khlopkov <k.hlopkov@gettransport.com>
2020-10-20 01:25:44 +0300

    Add guarantee page

---
+++ src/pages/Guarantee.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page padding>
+    <div>
+
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Guarantee',
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
