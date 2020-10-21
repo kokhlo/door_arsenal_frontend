ca97d1714c9e0ddba521611dccf83d5cbc6729fb
Konstantin Khlopkov <k.hlopkov@gettransport.com>
2020-10-21 23:10:06 +0300

    Delete and rename some files

--- src/plugins/currency-filter.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue';
-
-Vue.filter('dollar', (value) =>
-  // Using a template literal here, that's why there are two dollar signs.
-  // The first is an actual dollar.
-  `$${parseFloat(value).toFixed(2)}`);
